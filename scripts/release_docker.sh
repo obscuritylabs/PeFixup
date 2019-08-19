39d4f2ddd01df8362eaef8a5d1a99d40e5aed936
alexander rymdeko-harvey <a.rymdekoharvey@obscuritylabs.com>
2019-08-19 15:36:54 +0300

    Add docker image push to CI

--- scripts/release_docker.sh
+++ scripts/release_docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 VERSION=`cat VERSION`
 echo "---------------------------------------------"
@@ -11,13 +10,26 @@ echo "---------------------------------------------"
 
 # DOCKER TAG/VERSIONING
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
+git_sha="$(git rev-parse --short HEAD)"
 
 # PUSH TO DOCKER HUB
-docker push $USERNAME/$IMAGE:latest
-echo "------------------------------------------------"
-echo "| Docker image pushed: $USERNAME/$IMAGE:latest |"
-echo "------------------------------------------------"
-docker push $USERNAME/$IMAGE:$VERSION
-echo "--------------------------------------------------"
-echo "| Docker image pushed: $USERNAME/$IMAGE:$VERSION |"
-echo "--------------------------------------------------"
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    docker push $USERNAME/$IMAGE:latest
+	echo "------------------------------------------------"
+	echo "| Docker image pushed: $USERNAME/$IMAGE:latest |"
+	echo "------------------------------------------------"
+	docker push $USERNAME/$IMAGE:$VERSION
+	echo "--------------------------------------------------"
+	echo "| Docker image pushed: $USERNAME/$IMAGE:$VERSION |"
+	echo "--------------------------------------------------"
+fi
+if [[ "$TRAVIS_BRANCH" == "development" ]]; then
+    docker push $USERNAME/$IMAGE:development
+	echo "------------------------------------------------"
+	echo "| Docker image pushed: $USERNAME/$IMAGE:latest |"
+	echo "------------------------------------------------"
+	docker push $USERNAME/$IMAGE:${git_sha}-development
+	echo "----------------------------------------------------------------"
+	echo "| Docker image pushed: $USERNAME/$IMAGE:${git_sha}-development |"
+	echo "----------------------------------------------------------------"
+fi
